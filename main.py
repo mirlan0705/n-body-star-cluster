d64f01c2a78b1ff616ae8c304df9043ac9ac139b
amirlan <imsid1941@gmail.com>
2026-03-17 14:30:27 +0000

    finished logic

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ stars = []
 for i in range(50):
     x = random.uniform(100, 700)
     y = random.uniform(100, 700)
-    vx = random.uniform(-0.5, 0.5)
-    vy = random.uniform(-0.5, 0.5)
+    vx = random.uniform(-0.1, 0.1)
+    vy = random.uniform(-0.1, 0.1)
     mass = random.uniform(1, 3)
     stars.append([x,y,vx,vy,mass])
 
@@ -32,6 +32,7 @@ while running:
     screen.fill((0,0,0))
     for star in stars:
         for other in stars:
+            
             dx = other[0] - star[0]
             dy = other[1] - star[1]
 
@@ -41,12 +42,15 @@ while running:
             nx = dx/distance
             ny = dy/distance
 
-            ax = 100 * other[4]/distance**2 * nx
-            ay = 100 * other[4]/distance**2 * ny
+            ax = 0.5 * other[4]/distance**2 * nx
+            ay = 0.5 * other[4]/distance**2 * ny
 
-            star[0] += ax
-            star[1] += ay
-            pygame.draw.circle(screen, (255,255,255), (int(star[0]), int(star[1])), 2)
+            star[2] += ax
+            star[3] += ay
+
+        star[0] += star[2]
+        star[1] += star[3]
+        pygame.draw.circle(screen, (255,255,255), (int(star[0]), int(star[1])), 2)
     
     pygame.display.flip()
     clock.tick(60)
